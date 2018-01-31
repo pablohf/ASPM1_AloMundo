09e8610b122a6da10f650db066dd6814f6975af3
Pablo <Pablo@PABLO-PC>
2018-01-31 14:43:48 -0200

    Adicionar arquivos de projeto.

---
+++ ASPM1_AloMundo/AloMundo.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPM1_AloMundo
+{
+    public partial class AloMundo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
